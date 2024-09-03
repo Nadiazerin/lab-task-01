51f0561e6432c6a0756e496aa019531be4c90710
Nadia zerin <nadiajerin10@gmail.com>
2024-09-03 22:39:39 +0600

    basic python

---
+++ index.py
@@ -0,0 +1,116 @@
+#python syntax
+print ("hello world!")
+
+#variables in python
+
+x= 10
+x="bangladesh"
+print (x)
+print (type(x))
+
+#multiple variables
+
+x, y, z= "apple"  "banana" "cherry"
+
+print (x,y,z)
+
+#another
+names = ["zerin" "Ritu" "Saba"]
+x,y,z=names 
+print (x,y,z)
+
+#getting input from the users
+
+
+x= input ("Enter the first number")
+y=input ("Enter the second number")
+add= (x+y)
+print= add
+
+
+#multiple input from the user
+a,b=input ().split()
+print(a,b)
+
+
+#output variables
+
+X= "python"
+Y= "is"
+Z="awesome"
+print= (X+Y+Z)
+
+#list
+
+mylist=  ["apple" "banana" "cherry"]
+print (mylist)
+
+
+#access list item
+
+thislist=["zerin""ritu" "orange"] 
+print (thislist[1])
+
+
+
+#another 
+anotherList=[1, 4, 6, 7, 8, 9 , 9,6,]
+print (anotherList [2:6])
+
+
+#change list item 
+Thislist=(5,6,7,8,9,1,2,3,4,5)
+Thislist [1]=10
+
+
+#add list item 
+Thislist= ("apple ""orange" "banana" "kiwi" "mango" "lichi")
+Thislist.append("malta")
+print (Thislist)
+
+
+#insert item 
+Thislist= ("apple ""orange" "banana" "kiwi" "mango" "lichi")
+Thislist .insert (1,"grape")
+print (Thislist)
+
+
+#Extend list
+Thislist= ("apple ""orange" "banana" "kiwi" "mango" "lichi")
+toropical= ("pineapple" "papaya")
+thislist .Extend (toropical)
+print (Thislist)
+
+#remove list item 
+
+names= ["xerin" "Orin" "Ritu" "Saba" "Najifa"]
+names.remove ("xerin")
+print (names)
+
+
+#pop method 
+Thislist=(5,6,7,8,9,1,2,3,4,5)
+Thislist .pop (1)
+print (Thislist)
+
+
+#another 
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+#del method 
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+#another
+thislist = ["apple", "banana", "cherry"]
+thislist.clear ()
+print (thislist)
+
+#loop list 
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+ print (x)
+
